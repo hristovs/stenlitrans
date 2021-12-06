155a9658bf0c394063d54e1ac92e4500b1f5024f
hristovs <hristovs@tcd.ie>
2021-12-06 23:37:12 +0000

    tutorials

---
+++ stenlitrans/src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1> StenliTrans </h1>
+            <div> Links </div>
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
